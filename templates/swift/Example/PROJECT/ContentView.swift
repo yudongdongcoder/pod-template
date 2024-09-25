edf261733c1f6f3c73647d4ad80c85b51e785127
yudongdong <yudongdongcoder@gmail.com>
2024-09-25 16:20:42 +0800

    集成SwiftUI 逻辑

---
+++ templates/swift/Example/PROJECT/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  PROJECT_Example
+//
+//  Created by dongdong on 2024/9/25.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- templates/swift/Example/PROJECT/ViewController.swift
+++ templates/swift/Example/PROJECT/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swiftUIView = ContentView()
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        hostingController.didMove(toParent: self)
     }
 
     override func didReceiveMemoryWarning() {
