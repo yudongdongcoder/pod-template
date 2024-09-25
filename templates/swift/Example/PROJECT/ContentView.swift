679c27f8a7b9c82143b5aaf7cc5ffe7ad23dfe1f
yudongdong <yudongdongcoder@gmail.com>
2024-09-25 14:27:25 +0800

    创建SwiftUI模版

---
+++ templates/swift/Example/PROJECT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PROJECT
+//
+//  Created by dongdong on 2024/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ templates/swift/Example/PROJECT/PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROJECTApp.swift
+//  PROJECT
+//
+//  Created by dongdong on 2024/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
